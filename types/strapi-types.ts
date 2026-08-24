324525139f7ef580db8d8ad2d1954e06f9e86c19
Antoine Guglielmi <contact.antoine.guglielmi@gmail.com>
2026-08-25 00:47:40 +0200

    037 - Update rental and reference

--- types/strapi-types.ts
+++ types/strapi-types.ts
@@ -7,7 +7,6 @@ export interface Reference {
   publishedAt?: Date | string;
   locale?: string | null;
   rental?: Rental | null;
-  tenant?: Tenant | null;
   comment?: string;
   paidOnTime?: "yes" | "no" | "skip";
   wellMaintained?: "yes" | "no" | "skip";
@@ -28,10 +27,11 @@ export interface Rental {
   rentalToken?: string;
   expiresAt?: Date | string;
   landlordEmail?: string;
-  tenantDocumentId?: string;
   validatedAt?: Date | string;
   state?: "pending" | "validated";
   cityPublic?: string;
+  tenantVerification?: TenantVerification | null;
+  tenant?: Tenant | null;
 };
 
 export interface Tenant {
